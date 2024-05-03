6d0a25efba463d8c0eee4dadf508405d463d438a
patrociuq123 <167896247+patrociuq123@users.noreply.github.com>
2024-05-02 22:30:40 -0300

    Atualizar o Things.lua

--- Things.lua
+++ Things.lua
@@ -3,3 +3,8 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/tha
 local Window = OrionLib:MakeWindow({Name = "DestHub Beta", HidePremium = false, IntroEnabled = true, IntroText = "DestHub Beta Testing", SaveConfig = true, ConfigFolder = "DestroyerHub"})
 
 
+local TabBeta = Window:MakeTab({
+        Name = "Beta",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+})
