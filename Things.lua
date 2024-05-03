a7262d609b11350c695fa5847fc677777f617b0d
patrociuq123 <167896247+patrociuq123@users.noreply.github.com>
2024-05-02 22:25:29 -0300

    Atualizar o Things.lua

--- Things.lua
+++ Things.lua
@@ -1,1 +1,5 @@
-print("hello world")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/thanhdat4461/OrionMoblie/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "DestHub Beta", HidePremium = false, IntroEnabled = true, IntroText = "DestHub Beta Testing", SaveConfig = true, ConfigFolder = "DestroyerHub"})
+
+
