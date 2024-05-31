1c778f1a01dda0445251f676ffbbb22deb62ad9a
patrociuq123 <167896247+patrociuq123@users.noreply.github.com>
2024-05-31 20:30:50 -0300

    update DestroyerUiLib.lua

--- DestroyerUiLib.lua
+++ DestroyerUiLib.lua
@@ -296,8 +296,9 @@ end)
 CreateElement("Frame", function(Color)
 	local Frame = Create("Frame", {
 		BackgroundColor3 = Color or Color3.fromRGB(255, 255, 255),
+		BackgroundTransparency = 0.3,
 		BorderSizePixel = 0
-		BackgroundTransparency = 0.3
+
 	})
 	return Frame
 end)
